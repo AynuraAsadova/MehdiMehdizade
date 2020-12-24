cb6de5bbd5d07961514d82463f1b3793ef812aef
AynuraAsadova <65907499+AynuraAsadova@users.noreply.github.com>
2020-12-24 18:59:27 +0400

    Update title.js

--- js/title.js
+++ js/title.js
@@ -1,11 +1,1 @@
-// if(document.URL = "http://127.0.0.1:5500/full-news.html"){
-//     document.title = document.getElementById("text_title").innerHTML;
-// } else {
-//   document.title = "Mehdi Mehdizadə";
-// }
 
-if(document.URL != "https://aynuraasadova.github.io/MehdiMehdizade/full-news.html"){
-  document.title = "Mehdi Mehdizade"
-}else{
-  document.title = document.getElementById("text_title").innerHTML;
-}
